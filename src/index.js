4118089af4562a4f36815826171c0dc2002d5356
adstr123 <adam.matheson@ntlworld.com>
2018-10-14 17:36:00 +0100

    Included CSS in index.js, end of chapter 5

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@
 import React from 'react';
 // load one method from the react-dom library, to enable rendering React to HTML (can also be used for apps etc.)
 import { render } from 'react-dom'
+// instruct Webpack to take everything inside the style.css file and add it to the page in a style tag
+import './css/style.css'
 
 // import the component from its file
 import StorePicker from './components/StorePicker';
