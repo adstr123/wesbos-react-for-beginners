9e3fecfaaa8132eac5d89e30bae5b99fc7f2151e
adstr123 <adam.matheson@ntlworld.com>
2018-10-14 17:21:27 +0100

    Updated StorePicker with a form, beginning to learn JSX rules, end of chapter 4

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -6,7 +6,18 @@ class StorePicker extends React.Component {
 	// every component needs this function
 	// ES6 - same as writing function render()
 	render() {
-		return <p>Hello</p>
+		return (
+			// you can only ever return one parent element
+			<form className="store-selector">
+				<h2>Please Enter A Store</h2>
+				<input type="text" required placeholder="Store Name"/>
+				<button type="submit">Visit Store</button>
+			</form>
+		);
+
+		// example w/out JSX...
+		// return React.createElement('p', {className: 'Testing'}, 'I love you')
+		// ... renders a p element with Testing class, with the content 'I love you'
 	}
 }
 
