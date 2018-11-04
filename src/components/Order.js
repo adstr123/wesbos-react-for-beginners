2d2a4bbb195a1a9bb1ef9a2954f1a791f67fd411
adstr123 <adam.matheson@ntlworld.com>
2018-11-04 18:13:13 +0000

    Displayed orders with JSX, by passing fish & order as props to Order.js, end of chapter 17

--- src/components/Order.js
+++ src/components/Order.js
@@ -1,9 +1,57 @@
 import React from 'react';
+import { formatPrice } from '../helpers';
 
 class Order extends React.Component {
-	render() {
+	constructor() {
+		super();
+		this.renderOrder = this.renderOrder.bind(this);
+	}
+
+	renderOrder(key) {
+		const fish = this.props.fishes[key];
+		const count = this.props.order[key];
+
+		if (!fish || fish.status === "unavailable") {
+			// two name options: if fish exists in inventory but has stock 0, or if fish is totally removed from inventory management system
+			return <li key={key}>Sorry, {fish ? fish.name : 'fish'} is no longer available!></li>
+		}
+
 		return (
-			<p>Order</p>
+			<li key={key}>
+				<span>{count}lbs {fish.name}</span>
+				<span className="price">{formatPrice(count * fish.price)}</span>
+			</li>
+		)
+	}
+
+	render() {
+		// array of all the order keys (passed down as a prop)
+		const orderIds = Object.keys(this.props.order);
+		// total for the order - reduce executes defined function on each array el, outputting single value
+		const total = orderIds.reduce((prevTotal, key) => {
+			const fish = this.props.fishes[key];
+			const count = this.props.order[key];
+			const isAvailable = fish && fish.status === "available";
+			// || 0 for if a fish is in the order, then becomes unavailable (last fish)
+			if (isAvailable) {
+				return prevTotal + (count * fish.price || 0)
+			}
+			return prevTotal;
+		}, 0);
+		// starting reduce value of 0
+
+		return (
+			<div className="order-wrap">
+				<h2>Your Order</h2>
+				<ul className="order">
+					{/* outsource to another render function because there's a lot going on here */}
+					{orderIds.map(this.renderOrder)}
+					<li className="total">
+						<strong>Total:</strong>
+						{formatPrice(total)}
+					</li>
+				</ul>
+			</div>
 		)
 	}
 }
